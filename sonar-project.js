b7d8c080c6a16c9443e228d963254fd6a7f9a855
JojoJ27 <134658951+JojoJ27@users.noreply.github.com>
2023-07-06 18:12:54 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': 'admin',
+	    'sonar.login': '65252b9a5c1d4d7107093deaa95669f7d0e2a138',
 	    'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
